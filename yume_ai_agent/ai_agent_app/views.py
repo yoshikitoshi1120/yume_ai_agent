6f413b024b77cfd1f457508e3718b6fa29a90734
yoshiki <Pp5Dg5Vf0Pt9Ie0@hotmail.com>
2025-01-19 00:18:22 +0800

    add allow host

--- yume_ai_agent/ai_agent_app/views.py
+++ yume_ai_agent/ai_agent_app/views.py
@@ -1,11 +1,13 @@
 import logging
 
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from celery.result import AsyncResult
 from .tasks import generate_ai_response
 
 @require_http_methods(["POST"])
+@csrf_exempt
 def start_ai_response(request):
     uuid = request.POST.get('uuid', None)
     if not uuid:
@@ -19,6 +21,7 @@ logger = logging.getLogger(__name__)
 
 
 @require_http_methods(["GET"])
+@csrf_exempt
 def get_task_result(request, task_id):
     try:
         task = AsyncResult(task_id)
