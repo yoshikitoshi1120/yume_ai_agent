fcee8e2d019d2a7ba39b8291d950b9bde85f4f09
joe <adventurouscpl96@gmail.com>
2025-02-04 23:34:49 +0800

    fix: revert urls change.

--- yume_ai_agent/yume_ai_agent/urls.py
+++ yume_ai_agent/yume_ai_agent/urls.py
@@ -18,7 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ai_agent_app/', include('ai_agent_app.urls')),
